b19ffd418930fd2f34daa8c8983e34a2e8468c8c
SIDDHARDH PANNEERSELVAM <115604322+Siddhardhunkown@users.noreply.github.com>
2025-12-26 15:51:21 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+@app.route("/user/<name>")
+def user(name):
+    return f"Welcome {name}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
